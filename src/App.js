436f2575ebb4938b3e3477cdb168b42cd6bce303
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-03-30 15:51:57 -0700

    App component now renders Navbar and Calculator components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import NavBar from './components/NavBar';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="container calculator-container">
+        <Calculator />
+      </div>
     </div>
   );
 }
