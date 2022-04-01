dbe6bc883f72e2fa14e7592d5d375f51069c7808
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-03-31 17:32:17 -0700

    Switched React render method to pre v18 ReactDOM.render in order to pass FCC tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom/client';
+// import * as ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
 // Import Styles
@@ -9,12 +10,15 @@ import './index.css';
 
 import App from './App';
 
-// Attach the react component tree to the HTML DOM
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Attach the react component tree to the HTML DOM (React 18)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// FCC Testing Suite only performs properly using older render method
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
