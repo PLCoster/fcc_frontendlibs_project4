ce23f7b30400c9a3e6bea7db44789de0cb2ea894
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-04-01 17:14:10 -0700

    Updated button styles for calculator
    Standard, hover and active styling added for all buttons

--- src/components/ButtonsDisplay.js
+++ src/components/ButtonsDisplay.js
@@ -1,46 +1,110 @@
 import './ButtonsDisplay.css';
 
 const buttonInfo = [
-  { val: '(', id: 'left-parens', handler: 'handleParensPress' },
-  { val: ')', id: 'right-parens', handler: 'handleParensPress' },
-  { val: 'DEL', id: 'delete', handler: 'handleDeletePress' },
-  { val: 'CE', id: 'clear-entry', handler: 'handleClearEntryPress' },
-  { val: 'AC', id: 'clear', handler: 'handleAllClearPress' },
-  { val: '7', id: 'seven', handler: 'handleNumberPress' },
-  { val: '8', id: 'eight', handler: 'handleNumberPress' },
-  { val: '9', id: 'nine', handler: 'handleNumberPress' },
-  { val: '^', id: 'power', handler: 'handleOperationPress' },
-  { val: 'ln', id: 'log', handler: 'handleLogPress' },
-  { val: '4', id: 'four', handler: 'handleNumberPress' },
-  { val: '5', id: 'five', handler: 'handleNumberPress' },
-  { val: '6', id: 'six', handler: 'handleNumberPress' },
-  { val: '×', id: 'multiply', handler: 'handleOperationPress' },
-  { val: '÷', id: 'divide', handler: 'handleOperationPress' },
-  { val: '1', id: 'one', handler: 'handleNumberPress' },
-  { val: '2', id: 'two', handler: 'handleNumberPress' },
-  { val: '3', id: 'three', handler: 'handleNumberPress' },
-  { val: '+', id: 'add', handler: 'handleOperationPress' },
-  { val: '-', id: 'subtract', handler: 'handleOperationPress' },
-  { val: '+/-', id: 'negative', handler: 'handleNegativePress' },
-  { val: '0', id: 'zero', handler: 'handleNumberPress' },
-  { val: '.', id: 'decimal', handler: 'handleDecimalPress' },
+  {
+    val: '(',
+    id: 'left-parens',
+    handler: 'handleParensPress',
+    className: 'operator',
+  },
+  {
+    val: ')',
+    id: 'right-parens',
+    handler: 'handleParensPress',
+    className: 'operator',
+  },
+  {
+    val: 'DEL',
+    id: 'delete',
+    handler: 'handleDeletePress',
+    className: 'warning',
+    key: 'Delete',
+  },
+  {
+    val: 'CE',
+    id: 'clear-entry',
+    handler: 'handleClearEntryPress',
+    className: 'warning',
+  },
+  {
+    val: 'AC',
+    id: 'clear',
+    handler: 'handleAllClearPress',
+    key: 'Escape',
+    className: 'warning',
+  },
+  { val: '7', id: 'seven', handler: 'handleNumberPress', className: 'number' },
+  { val: '8', id: 'eight', handler: 'handleNumberPress', className: 'number' },
+  { val: '9', id: 'nine', handler: 'handleNumberPress', className: 'number' },
+  {
+    val: '^',
+    id: 'power',
+    handler: 'handleOperationPress',
+    className: 'operator',
+  },
+  { val: 'ln', id: 'log', handler: 'handleLogPress', className: 'operator' },
+  { val: '4', id: 'four', handler: 'handleNumberPress', className: 'number' },
+  { val: '5', id: 'five', handler: 'handleNumberPress', className: 'number' },
+  { val: '6', id: 'six', handler: 'handleNumberPress', className: 'number' },
+  {
+    val: '×',
+    id: 'multiply',
+    handler: 'handleOperationPress',
+    className: 'operator',
+    key: '*',
+  },
+  {
+    val: '÷',
+    id: 'divide',
+    handler: 'handleOperationPress',
+    className: 'operator',
+    key: '/',
+  },
+  { val: '1', id: 'one', handler: 'handleNumberPress', className: 'number' },
+  { val: '2', id: 'two', handler: 'handleNumberPress', className: 'number' },
+  { val: '3', id: 'three', handler: 'handleNumberPress', className: 'number' },
+  {
+    val: '+',
+    id: 'add',
+    handler: 'handleOperationPress',
+    className: 'operator',
+  },
+  {
+    val: '-',
+    id: 'subtract',
+    handler: 'handleOperationPress',
+    className: 'operator',
+  },
+  {
+    val: '+/-',
+    id: 'negative',
+    handler: 'handleNegativePress',
+    className: 'number',
+  },
+  { val: '0', id: 'zero', handler: 'handleNumberPress', className: 'number' },
+  {
+    val: '.',
+    id: 'decimal',
+    handler: 'handleDecimalPress',
+    className: 'number',
+  },
   { val: '=', id: 'equals', handler: 'handleEqualPress', className: 'equals' },
 ];
 
 function ButtonsDisplay({ keyPressed, handlers, syntaxError }) {
-  console.log('Button Display');
-  const buttons = buttonInfo.map(({ val, id, handler, className }) => {
+  const buttons = buttonInfo.map(({ val, id, handler, className, key }) => {
+    const activeClass = (key || val) === keyPressed ? ' active' : '';
     return (
       <button
         id={id}
         key={id}
-        className={className}
+        className={className + activeClass}
         onClick={() => {
           console.log(id, 'clicked', syntaxError);
           syntaxError ? handlers.handleAllClearPress() : handlers[handler](val);
         }}
       >
-        {val}
+        <h5>{val}</h5>
       </button>
     );
   });
