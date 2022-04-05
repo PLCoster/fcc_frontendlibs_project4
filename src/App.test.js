b0a733cb6955cd51d0eac60f1a871978e67c9abe
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-04-04 21:27:34 -0700

    Added additional test cases for:
    History Display
    Decimal Input
    Negative number input
    Parentheses Logic

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import {
+  render,
+  screen,
+  fireEvent,
+  getByText,
+  queryByText,
+} from '@testing-library/react';
 import App from './App';
 
 describe('App renders desired basic elements on screen', () => {
-  test('renders Calculator App components correctly', () => {
+  test('Renders Calculator App components correctly', () => {
     const { container: app } = render(<App />);
     // console.log('App is: ', app);
     const calculatorBody = app.querySelector('.calculator-body');
@@ -11,7 +17,7 @@ describe('App renders desired basic elements on screen', () => {
     expect(historyDisplay).toBeInTheDocument();
   });
 
-  test('renders Calculator Buttons correctly', () => {
+  test('Renders Calculator Buttons correctly', () => {
     render(<App />);
     const oneButton = screen.getByText('1');
     expect(oneButton).toBeInTheDocument();
@@ -35,7 +41,7 @@ describe('App renders desired basic elements on screen', () => {
     expect(zeroButton).toBeInTheDocument();
   });
 
-  test('renders Operation Buttons correctly', () => {
+  test('Renders Operation Buttons correctly', () => {
     render(<App />);
     const leftParens = screen.getByText('(');
     expect(leftParens).toBeInTheDocument();
@@ -96,19 +102,24 @@ const buttonsArray = [
   '=',
 ];
 
-const buttonsReducer = (accum, buttonStr) => {
-  if (buttonStr !== 0) {
-    accum[buttonStr] = screen.getByText(buttonStr);
-  } else {
-    accum[buttonStr] = screen.getAllByText(buttonStr)[1];
-  }
-  return accum;
+const initializeApp = () => {
+  render(<App />);
+
+  const buttonsReducer = (accum, buttonStr) => {
+    if (buttonStr !== 0) {
+      accum[buttonStr] = screen.getByText(buttonStr);
+    } else {
+      accum[buttonStr] = screen.getAllByText(buttonStr)[1];
+    }
+    return accum;
+  };
+
+  return buttonsArray.reduce(buttonsReducer, {});
 };
 
-describe('has basic calculator functionality', () => {
-  test('basic number entry function works correctly', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+describe('Has basic calculator functionality', () => {
+  test('Basic number entry function works correctly', () => {
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
     // Type 159 into the calculator, it should show on the number display
     fireEvent.click(buttons[1]);
@@ -117,9 +128,51 @@ describe('has basic calculator functionality', () => {
     expect(numberDisplay).toHaveTextContent('159');
   });
 
-  test('clear entry button clears number display', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+  test('Decimal number entry works correctly', () => {
+    const buttons = initializeApp();
+    const numberDisplay = screen.getByTestId('number-display');
+    // Type 159 into the calculator, it should show on the number display
+    fireEvent.click(buttons['.']);
+
+    expect(numberDisplay).toHaveTextContent('0.');
+
+    fireEvent.click(buttons['.']);
+    expect(numberDisplay).toHaveTextContent('0.');
+
+    fireEvent.click(buttons[0]);
+    fireEvent.click(buttons[1]);
+    fireEvent.click(buttons['.']);
+    expect(numberDisplay).toHaveTextContent('0.01');
+  });
+
+  test('Negative number entry works correctly', () => {
+    const buttons = initializeApp();
+    const numberDisplay = screen.getByTestId('number-display');
+    const formulaDisplay = screen.getByTestId('formula-display');
+
+    // Type 159 into the calculator, it should show on the number display
+    fireEvent.click(buttons['+/-']);
+    expect(numberDisplay).toHaveTextContent('0');
+
+    fireEvent.click(buttons['5']);
+    expect(numberDisplay).toHaveTextContent('5');
+
+    fireEvent.click(buttons['+/-']);
+    expect(numberDisplay).toHaveTextContent('-5');
+
+    fireEvent.click(buttons['+/-']);
+    expect(numberDisplay).toHaveTextContent('5');
+
+    fireEvent.click(buttons['+/-']);
+    expect(numberDisplay).toHaveTextContent('-5');
+
+    fireEvent.click(buttons['+']);
+    expect(numberDisplay).toHaveTextContent('0');
+    expect(formulaDisplay).toHaveTextContent('(-5) +');
+  });
+
+  test('Clear entry button clears number display', () => {
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
 
     // Type 159 into the calculator, it should show on the number display
@@ -134,8 +187,7 @@ describe('has basic calculator functionality', () => {
   });
 
   test('Adds two numbers correctly and displays the result', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
     const formulaDisplay = screen.getByTestId('formula-display');
 
@@ -153,8 +205,7 @@ describe('has basic calculator functionality', () => {
   });
 
   test('Subtracts two numbers correctly and displays the result', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
     const formulaDisplay = screen.getByTestId('formula-display');
 
@@ -172,8 +223,7 @@ describe('has basic calculator functionality', () => {
   });
 
   test('Multiplies two numbers correctly and displays the result', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
     const formulaDisplay = screen.getByTestId('formula-display');
 
@@ -191,8 +241,7 @@ describe('has basic calculator functionality', () => {
   });
 
   test('Divides two numbers correctly and displays the result', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
     const formulaDisplay = screen.getByTestId('formula-display');
 
@@ -210,8 +259,7 @@ describe('has basic calculator functionality', () => {
   });
 
   test('Calculates simple powers correctly and displays the result', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
     const formulaDisplay = screen.getByTestId('formula-display');
 
@@ -229,8 +277,7 @@ describe('has basic calculator functionality', () => {
   });
 
   test('Calculates logarithms correctly and displays the result', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
     const formulaDisplay = screen.getByTestId('formula-display');
 
@@ -246,9 +293,8 @@ describe('has basic calculator functionality', () => {
     expect(formulaDisplay).toHaveTextContent('ln(9) =');
   });
 
-  test('all clear button clears number and formula', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+  test('All clear button clears number and formula', () => {
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
     const formulaDisplay = screen.getByTestId('formula-display');
 
@@ -269,9 +315,8 @@ describe('has basic calculator functionality', () => {
     expect(formulaDisplay).toHaveTextContent('');
   });
 
-  test('calculates more complicated input correctly', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+  test('Calculates more complicated input correctly', () => {
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
     const formulaDisplay = screen.getByTestId('formula-display');
 
@@ -295,32 +340,8 @@ describe('has basic calculator functionality', () => {
     expect(formulaDisplay).toHaveTextContent('3 + 5 × 6 - 2 ÷ 4 =');
   });
 
-  test('all clear button clears number and formula', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
-    const numberDisplay = screen.getByTestId('number-display');
-    const formulaDisplay = screen.getByTestId('formula-display');
-
-    // Type 9 + 3 = into the calculator, result should be 12
-    fireEvent.click(buttons[9]);
-    fireEvent.click(buttons['+']);
-    fireEvent.click(buttons[3]);
-    fireEvent.click(buttons['=']);
-
-    // Number Display should read 12
-    expect(numberDisplay).toHaveTextContent('12');
-
-    // Formula Display should read '9 + 3 ='
-    expect(formulaDisplay).toHaveTextContent('9 + 3 =');
-
-    fireEvent.click(buttons['AC']);
-    expect(numberDisplay).toHaveTextContent('0');
-    expect(formulaDisplay).toHaveTextContent('');
-  });
-
-  test('parenthesis are interpreted correctly in formulae', () => {
-    render(<App />);
-    const buttons = buttonsArray.reduce(buttonsReducer, {});
+  test('Parenthesis are interpreted correctly in formulae', () => {
+    const buttons = initializeApp();
     const numberDisplay = screen.getByTestId('number-display');
     const formulaDisplay = screen.getByTestId('formula-display');
 
@@ -344,5 +365,83 @@ describe('has basic calculator functionality', () => {
 
     // Formula Display should read '9 + 3 ='
     expect(formulaDisplay).toHaveTextContent('(3 + 5) × 6 - 2 ÷ 4 =');
+  });
+
+  test('Parentheses can only be entered in logical way', () => {
+    const buttons = initializeApp();
+    const numberDisplay = screen.getByTestId('number-display');
+    const formulaDisplay = screen.getByTestId('formula-display');
+
+    // Type ))(3 + 5)) x 6 - 2 / 4 = into calculator, result is 47.5
+    // (Formula / Expression Logic)
+
+    // Nonsensical closing parens
+    fireEvent.click(buttons[')']);
+    fireEvent.click(buttons[')']);
+
+    fireEvent.click(buttons['(']);
+    fireEvent.click(buttons[3]);
+    fireEvent.click(buttons['+']);
+    fireEvent.click(buttons[5]);
+
+    fireEvent.click(buttons[')']);
+
+    // Superflous closing parens
+    fireEvent.click(buttons[')']);
+    fireEvent.click(buttons[')']);
+
+    fireEvent.click(buttons['×']);
+    fireEvent.click(buttons['6']);
+    fireEvent.click(buttons['-']);
+    fireEvent.click(buttons['2']);
+    fireEvent.click(buttons['÷']);
+    fireEvent.click(buttons['4']);
+    fireEvent.click(buttons['=']);
+
+    // Number Display should read 32.5
+    expect(numberDisplay).toHaveTextContent('47.5');
+
+    // Formula Display should read '9 + 3 ='
+    expect(formulaDisplay).toHaveTextContent('(3 + 5) × 6 - 2 ÷ 4 =');
+  });
+});
+
+describe('history display functions corrrectly', () => {
+  test('History display is rendered by App', () => {
+    const buttons = initializeApp();
+    const historyDisplay = screen.getByText('History:');
+    console.log('HISTORY DISPLAY: ', historyDisplay.nextSibling);
+    expect(historyDisplay).toBeInTheDocument();
+  });
+
+  test('History is updated when a calculation is made', () => {
+    const buttons = initializeApp();
+    const historyDisplay =
+      screen.getByText('History:').parentElement.parentElement;
+
+    fireEvent.click(buttons[9]);
+    fireEvent.click(buttons['+']);
+    fireEvent.click(buttons[3]);
+    fireEvent.click(buttons['=']);
+
+    expect(getByText(historyDisplay, '9 + 3 =')).toBeInTheDocument();
+  });
+
+  test('History is cleared after clicking history clear button', () => {
+    const buttons = initializeApp();
+    const historyDisplay =
+      screen.getByText('History:').parentElement.parentElement;
+    const historyClearButton = screen.getByText('History:').nextSibling;
+
+    fireEvent.click(buttons[9]);
+    fireEvent.click(buttons['+']);
+    fireEvent.click(buttons[3]);
+    fireEvent.click(buttons['=']);
+
+    expect(getByText(historyDisplay, '9 + 3 =')).toBeInTheDocument();
+
+    fireEvent.click(historyClearButton);
+
+    expect(queryByText(historyDisplay, '9 + 3 =')).toBeNull();
   });
 });
